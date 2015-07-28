df9683ff2a628e395743b6b7ca3115d52a8ff8b7
james-smith-za <smi.james.th@gmail.com>
2015-07-28 09:29:10 +0200

    added plotting functionality, seems to work

--- hdf5/receiver.py
+++ hdf5/receiver.py
@@ -12,6 +12,15 @@ import time
 import os
 import numpy as np
 
+import collections
+
+import matplotlib
+matplotlib.use('gtkagg') # Necessary on optimus for some reason - older version of matplotlib.
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from matplotlib.widgets import Slider
+
+data_width = 1024
 
 ###################### Multiprocessing shared values ######################
 script_run = multiprocessing.Value('B', 1) # Boolean to keep track of whether the program shold actually run, initialise to 1
@@ -25,6 +34,10 @@ frame_len = multiprocessing.Value('H', 0)
 frame_number = multiprocessing.Value('L', 0)
 good_frames = multiprocessing.Value('L', 0)
 bad_frames = multiprocessing.Value('L', 0)
+
+
+video_average_length = multiprocessing.Value('B', 10) # This is a bit of a hack, but it seems to work fine...
+
 
 ###################### Signal handler functions ######################
 def ctrl_c(signal, frame):
@@ -46,7 +59,6 @@ def UDP_receiver(output_queue):
     '''
     signal.signal(signal.SIGINT, signal.SIG_IGN) # Ignore keyboard interrupt signal, parent process will handle.
 
-    #log_file = open('udp_logfile', 'w')
 
     udp_process_priority = -19
     time.sleep(0.2) # Just to give the other processes time to print their messages so this doesn't get lost
@@ -78,6 +90,8 @@ def UDP_unpacker(input_queue, output_queue):
     signal.signal(signal.SIGINT, signal.SIG_IGN) # Ignore keyboard interrupt signal, parent process will handle.
     counter = 0 # Packets start with zero (hopefully)
 
+    log_file = open('udp_logfile', 'w')
+
     header_length = 16
     data = input_queue.get()
     if data == None:
@@ -87,10 +101,11 @@ def UDP_unpacker(input_queue, output_queue):
     packet_length = len(data)
     data_length = packet_length - header_length
     n_samples = data_length / 4
-    magic_no, frame_no, subframe_no, frame_size, mode = struct.unpack('!IQBBH%dx'%(data_length), data)
+    magic_no, frame_no, subframe_no, frame_size,  mode = struct.unpack('!IQBBH%dx'%(data_length), data)
     packet_data = struct.unpack('!%dx%di'%(header_length, n_samples), data)
 
     #log_file.write(' frame_no: %d subframe_no: %d frame_size: %d mode: %d '%(frame_no, subframe_no, frame_size, mode ))
+    #frame_size = 16 # REMEMBER TO REMOVE ONCE HARDCODED STUFF IS REMOVED!!
 
     if magic_no != 439041101:
         print 'Magic number unexpected value %d'%(magic_no)
@@ -98,14 +113,12 @@ def UDP_unpacker(input_queue, output_queue):
         sys.exit()
 
     data_mode.value = mode
-    pkt_len.value = packet_length
-    pkt_smpl_len.value = n_samples
-    #frame_len.value = frame_size
-    frame_len.value = 16
-    #fft_win_len.value = frame_size*n_samples
-    fft_win_len.value = 1024
-    #interleaved_window_len = data_length*frame_size / 4 # /4 to take into account that there are 4 bytes per element
-    interleaved_window_len = 4096
+    pkt_len.value = (packet_length)
+    pkt_smpl_len.value = (n_samples)
+    frame_len.value = (frame_size)
+    fft_win_len.value = (frame_size*n_samples)
+    interleaved_window_len = (data_length*frame_size) / 4 # /4 to take into account that there are 4 bytes per element
+    #interleaved_window_len = 4096
 
     counter += 1 # can't forget this, otherwise we miss the first frame (if we happen to catch the first packet, which is usually the case)
 
@@ -116,7 +129,7 @@ def UDP_unpacker(input_queue, output_queue):
         data = input_queue.get()
         if data == None:
             break
-        magic_no, frame_no, subframe_no, mode, frame_size = struct.unpack('!IQHBB%dx'%(data_length), data)
+        magic_no, frame_no, subframe_no, frame_size,  mode = struct.unpack('!IQBBH%dx'%(data_length), data)
         #log_file.write(' frame_no: %d subframe_no: %d frame_size: %d mode: %d '%(frame_no, subframe_no, frame_size, mode ))
         packet_data = struct.unpack('!%dx%di'%(header_length, n_samples), data)
 
@@ -162,11 +175,11 @@ def FFT_deinterleaver(input_queue, output_queue):
 
         even1 = (interleaved_window[0::8] + interleaved_window[1::8]*1j)
         odd1  = (interleaved_window[2::8] + interleaved_window[3::8]*1j)
-        LCP   = np.reshape(np.dstack((even1, odd1)), (1,-1))
+        LCP   = np.reshape(np.dstack((even1, odd1)), (1,-1))[0]
 
         even2 = (interleaved_window[4::8] + interleaved_window[5::8]*1j)
         odd2  = (interleaved_window[6::8] + interleaved_window[7::8]*1j)
-        RCP   = np.reshape(np.dstack((even2, odd2)), (1, -1))
+        RCP   = np.reshape(np.dstack((even2, odd2)), (1,-1))[0]
 
         #need to figure out here how to write out to the plotting function.
 
@@ -174,6 +187,93 @@ def FFT_deinterleaver(input_queue, output_queue):
 
     print 'deinterleaver found poison pill'
     output_queue.put(None)
+
+def queue_decimator(input_queue, output_queue, output_decimated_queue, decimation_factor = 300):
+    '''
+    Decimates the queue by the specified factor. Passes full data rate out to one queue for recording, and decimated data out to plotting queue.
+    '''
+    signal.signal(signal.SIGINT, signal.SIG_IGN) # Ignore keyboard interrupt signal, parent process will handle.
+    counter = 0
+    while 1:
+        input_tuple = input_queue.get()
+        if input_tuple == None:
+            break
+        output_queue.put(input_tuple)
+        counter += 1
+        if counter == decimation_factor:
+            counter = 0
+            timestamp, LCP, RCP = input_tuple
+            LCP_power = np.square(np.abs(LCP))
+            RCP_power = np.square(np.abs(RCP))
+            output_decimated_queue.put((LCP, RCP))
+
+    print 'queue decimator received poison pill'
+    output_queue.put(None)
+    output_decimated_queue.put(None)
+
+
+def plotter(input_queue):
+    '''
+    This process is supposed to handle the graphs.
+    Not pretty at the moment... but then matplotlib never is.
+    '''
+    signal.signal(signal.SIGINT, signal.SIG_IGN) # Ignore keyboard interrupt signal, parent process will handle.
+    waterfall_size = 150 # This makes it about 75 seconds in theory. Drawing the graph sometimes takes a bit longer.
+    fig = plt.figure(figsize=(20,15))
+    plt.subplots_adjust(left = 0.1, bottom = 0.25)
+    ax = plt.subplot(1, 1, 1)
+    line_lcp, = ax.plot([], [], 'b', lw=1)
+    line_rcp, = ax.plot([], [], 'r', lw=1)
+    ax.set_xlim(0,400)
+
+    vav_data_lcp = collections.deque(maxlen = waterfall_size)
+    vav_data_rcp = collections.deque(maxlen = waterfall_size)
+
+    slider_ax = plt.axes([0.25, 0.1, 0.65, 0.03])
+
+    video_average_length_slider = Slider(slider_ax, 'VAv', 1, waterfall_size, valinit=video_average_length.value)
+    def update(val):
+        video_average_length.value = int(video_average_length_slider.val)
+        fig.canvas.draw_idle()
+    video_average_length_slider.on_changed(update)
+
+    def init():
+        x = np.zeros(data_width)
+        y = np.zeros(data_width)
+        line_lcp.set_data(x,y)
+        line_rcp.set_data(x,y)
+        return line_lcp, line_rcp
+
+    def animate(*args):
+        if script_run.value != 1 and input_queue.qsize() == 0:
+            sys.exit()
+        x = np.arange(0, 400, 400.0 / data_width)
+        new_lcp, new_rcp = input_queue.get()
+        vav_data_lcp.appendleft(new_lcp)
+        vav_data_rcp.appendleft(new_rcp)
+        lcp = np.zeros(data_width)
+        rcp = np.zeros(data_width)
+        for i in range(video_average_length.value):
+            if i < len(vav_data_lcp):
+                lcp += np.array(vav_data_lcp[i])
+                rcp += np.array(vav_data_rcp[i])
+        lcp /= video_average_length.value
+        rcp /= video_average_length.value
+        graph_max = 0
+        if lcp.max() > rcp.max():
+            graph_max = lcp.max()
+        else:
+            graph_max = rcp.max()
+        ax.set_ylim(0,graph_max + 1)
+        line_lcp.set_data(x,lcp)
+        line_rcp.set_data(x,rcp)
+        return line_lcp,line_rcp
+
+    # Set the animation off to a start...
+    anim = animation.FuncAnimation(fig, animate, init_func=init, blit=True, interval=5)
+    plt.show()
+    print 'plotter process finished.'
+
 
 
 def dummy_queue_emptyer(input_queue):
@@ -187,7 +287,7 @@ def dummy_queue_emptyer(input_queue):
     print 'dummy received poison pill'
 
 
-def diagnostic_info(q1, q2, q3):
+def diagnostic_info(q1, q2, q3, q4, q5):
     signal.signal(signal.SIGINT, signal.SIG_IGN) # Ignore keyboard interrupt signal, parent process will handle.
     print 'PID: ', os.getpid()
     print 'Waiting for first packet...'
@@ -207,14 +307,16 @@ def diagnostic_info(q1, q2, q3):
     print printstr
     diagnose=True
 
-    while diagnose or q1.qsize() > 0 or q2.qsize() > 0:
-        printstr = '\r' +\
+    while diagnose or q1.qsize() > 0 or q2.qsize() > 0 or q3.qsize() > 0 or q4.qsize() > 0 or q5.qsize() > 0:
+        printstr = '\r' + str(diagnose) + \
                    '| Frame number:' + ('%d'%(frame_number.value)).rjust(12) +\
                    '| Good frames:' + ('%d'%(good_frames.value)).rjust(12) +\
                    '| Bad frames:' + ('%d'%(bad_frames.value)).rjust(12) +\
                    '| Q1 length:' + ('%d'%(q1.qsize())).rjust(12) +\
                    '| Q2 length:' + ('%d'%(q2.qsize())).rjust(12) +\
                    '| Q3 length:' + ('%d'%(q3.qsize())).rjust(12) +\
+                   '| Q4 length:' + ('%d'%(q4.qsize())).rjust(12) +\
+                   '| Q5 length:' + ('%d'%(q5.qsize())).rjust(12) +\
                    '| Frame drop percentage: %f'%(float(bad_frames.value) / (good_frames.value + bad_frames.value + 1) * 100)
         sys.stdout.write(printstr)
         sys.stdout.flush()
@@ -228,19 +330,25 @@ if __name__ == '__main__':
 
     receive_unpack_queue = multiprocessing.Queue()
     unpack_deint_queue = multiprocessing.Queue()
-    deint_storage_queue = multiprocessing.Queue()
+    deint_decimate_queue = multiprocessing.Queue()
+    storage_queue = multiprocessing.Queue()
+    plot_queue = multiprocessing.Queue()
 
     UDP_receiver_process = multiprocessing.Process(name='UDP receiver', target=UDP_receiver, args=(receive_unpack_queue,))
     UDP_unpacker_process = multiprocessing.Process(name='UDP receiver', target=UDP_unpacker, args=(receive_unpack_queue, unpack_deint_queue,))
     # This is where a decision should be made as to which deinterleaver process to start.
-    deinterleaver_process = multiprocessing.Process(name='deinterleaver', target=FFT_deinterleaver, args=(unpack_deint_queue, deint_storage_queue))
-    dummy_process = multiprocessing.Process(name='dummy', target=dummy_queue_emptyer, args=(deint_storage_queue,))
-    diagnostics_process = multiprocessing.Process(name='diagnostics', target=diagnostic_info, args=(receive_unpack_queue, unpack_deint_queue,deint_storage_queue))
+    deinterleaver_process = multiprocessing.Process(name='deinterleaver', target=FFT_deinterleaver, args=(unpack_deint_queue, deint_decimate_queue))
+    decimator_process = multiprocessing.Process(name='decimator', target=queue_decimator, args=(deint_decimate_queue, storage_queue, plot_queue))
+    plot_process = multiprocessing.Process(name='dummy', target=plotter, args=(plot_queue,))
+    dummy_storage_process = multiprocessing.Process(name='dummy', target=dummy_queue_emptyer, args=(storage_queue,))
+    diagnostics_process = multiprocessing.Process(name='diagnostics', target=diagnostic_info, args=(receive_unpack_queue, unpack_deint_queue, deint_decimate_queue, storage_queue, plot_queue))
 
     UDP_receiver_process.start()
     UDP_unpacker_process.start()
     deinterleaver_process.start()
-    dummy_process.start()
+    decimator_process.start()
+    plot_process.start()
+    dummy_storage_process.start()
     diagnostics_process.start()
 
     signal.pause()
@@ -248,7 +356,9 @@ if __name__ == '__main__':
     UDP_receiver_process.join()
     UDP_unpacker_process.join()
     deinterleaver_process.join()
-    dummy_process.join()
+    decimator_process.join()
+    plot_process.join()
+    dummy_storage_process.join()
     diagnostics_process.join()
 
 
